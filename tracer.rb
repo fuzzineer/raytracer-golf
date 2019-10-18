e6772807896d7dc3faddf7441ec3e71c69baf01d
Amy <amy@fluff.tech>
2019-10-18 18:05:41 -0400

    replace vector object with arrays (unoptimized)

--- tracer.rb
+++ tracer.rb
@@ -1,35 +1,25 @@
 require "chunky_png"
 
-class Vec3
-	attr_reader :x, :y, :z
-	def initialize(x, y = x, z = x)
-		@x, @y, @z = x, y, z
-	end
-	
-	def +(vec)
-		Vec3.new(x + vec.x, y + vec.y, z + vec.z)
-	end
-	def -@
-		Vec3.new(-x, -y, -z)
-	end
-	def *(fac)
-		Vec3.new(x * fac, y * fac, z * fac)
-	end
-	def /(fac)
-		self * (1.0 / fac)
-	end
-	def abs
-		dot(self)
-	end
-	def components
-		[x, y, z]
-	end
-	def normalize
-		self / Math.sqrt(abs)
-	end
-	def dot(vec)
-		x * vec.x + y * vec.y + z * vec.z
-	end
+def add_vec(a, b)
+	a.zip(b).map{|m,n|m+n}
+end
+def sub_vec(a, b)
+	a.zip(b).map{|m,n|m-n}
+end
+def mul_vec(a, f)
+	a.map{|m|m*f}
+end
+def div_vec(a, f)
+	mul_vec(a,1.0/f)
+end
+def dot_vec(a, b)
+	a.zip(b).map{|m,n|m*n}.sum
+end
+def abs(a)
+	dot_vec(a,a)
+end
+def norm(a)
+	div_vec(a,Math.sqrt(abs(a)))
 end
 
 class Sphere
@@ -42,10 +32,10 @@ class Sphere
 	end
 	
 	def intersect(ray_orig, ray_dir)
-		dist = ray_orig + -center
-		b = 2 * dist.dot(ray_dir)
+		dist = sub_vec(ray_orig, center)
+		b = 2 * dot_vec(dist, ray_dir)
 		return 1e8 if b > 0
-		c = dist.abs - radius ** 2
+		c = abs(dist) - radius ** 2
 		disc = (b ** 2) - (4 * c)
 		return 1e8 if disc < 0
 		sq = Math.sqrt(disc)
@@ -60,43 +50,42 @@ end
 
 class CheckeredSphere < Sphere
 	def color(intersect)
-		checker = (intersect.x.floor % 2) == (intersect.z.floor % 2)
-		color_vec * (checker ? 1 : 0)
+		checker = (intersect[0].floor % 2) == (intersect[2].floor % 2)
+		mul_vec(color_vec, checker ? 1 : 0)
 	end
 end
 
 def raytrace(ray_orig, ray_dir, world, depth = 0)
 	
-	min_dist, nearest_obj = world.to_h{|s| [s.intersect(ray_orig, ray_dir), s]}.min_by{|k,v| k}
+	nearest_obj, min_dist = world.to_h{|s| [s, s.intersect(ray_orig, ray_dir)]}.min_by{|k,v| v}
 	
-	return Vec3.new(0.5) if min_dist >= 1e8
+	return [0.5]*3 if min_dist >= 1e8
 	
-	intersect = ray_orig + ray_dir * min_dist
-	normal = ((intersect + -nearest_obj.center) / nearest_obj.radius).normalize
+	intersect = add_vec(ray_orig, mul_vec(ray_dir, min_dist))
+	normal = norm(div_vec(sub_vec(intersect, nearest_obj.center), nearest_obj.radius))
 	
-	color = Vec3.new(0.05)
+	color = [0.05]*3
 	
-	light_pos = Vec3.new(0, 20, 10)
+	light_pos = [0, 20, 10]
 	
-	light_dir = (light_pos + -intersect).normalize
-	origin_dir = (Vec3.new(0) + -intersect).normalize
+	light_dir = norm(sub_vec(light_pos, intersect))
+	origin_dir = norm(sub_vec([0]*3, intersect))
 	
-	offset = intersect + normal * 1e-4
+	offset = add_vec(intersect, mul_vec(normal, 1e-4))
 	
 	light_distances = world.map { |obj| obj.intersect(offset, light_dir) }
-	light_nearest = light_distances.min
-	light_visible = light_distances[world.index(nearest_obj)] == light_nearest
+	light_visible = light_distances[world.index(nearest_obj)] == light_distances.min
 	
-	lv = [0, normal.dot(light_dir)].max
-	color += nearest_obj.color(intersect) * lv if light_visible
+	lv = [0, dot_vec(normal, light_dir)].max
+	color = add_vec(color, mul_vec(nearest_obj.color(intersect), lv)) if light_visible
 	
 	if nearest_obj.reflect > 0 && depth < 5
-		reflect_ray_dir = (ray_dir + -normal * 2 * ray_dir.dot(normal)).normalize
-		color += raytrace(offset, reflect_ray_dir, world, depth + 1) * nearest_obj.reflect
+		reflect_ray_dir = norm(sub_vec(ray_dir, mul_vec(normal, 2 * dot_vec(ray_dir, normal))))
+		color = add_vec(color, mul_vec(raytrace(offset, reflect_ray_dir, world, depth + 1), nearest_obj.reflect))
 	end
 	
-	phong = normal.dot((light_dir + origin_dir).normalize)
-	color += Vec3.new(1) * (phong.clamp(0, 1) ** 50) if light_visible
+	phong = dot_vec(normal, norm(add_vec(light_dir, origin_dir)))
+	color = add_vec(color, mul_vec([1]*3, phong.clamp(0, 1) ** 50)) if light_visible
 	
 	return color
 end
@@ -112,11 +101,8 @@ def render(world)
 			x = (2 * ((col + 0.5) * (1.0 / 640)) - 1) * angle * aspect_ratio
 			y = (1 - 2 * ((row + 0.5) * (1.0 / 480))) * angle
 			
-			ray_orig = Vec3.new(0)
-			ray_dir = Vec3.new(x, y, 1).normalize
-			
-			color = raytrace(ray_orig, ray_dir, world)
-			image[col, row] = ChunkyPNG::Color.rgb(*color.components.map{|c| (c.clamp(0, 1) * 255).to_i})
+			color = raytrace([0]*3, norm([x, y, 1.0]), world)
+			image[col, row] = ChunkyPNG::Color.rgb(*color.map{|c| (c.clamp(0, 1) * 255).to_i})
 		end
 	end
 	
@@ -124,9 +110,9 @@ def render(world)
 end
 
 world = [
-	CheckeredSphere.new(Vec3.new(0, -10004, 20), 10000, Vec3.new(0.25), 0.2),
-	Sphere.new(Vec3.new(0, 0, 20), 4, Vec3.new(1, 0, 0), 0.2),
-	Sphere.new(Vec3.new(6, -1, 20), 2, Vec3.new(0, 0, 1), 0.2),
+	CheckeredSphere.new([0, -10004, 20], 10000, [0.25]*3, 0.2),
+	Sphere.new([0, 0, 20], 4, [1, 0, 0], 0.2),
+	Sphere.new([6, -1, 20], 2, [0, 0, 1], 0.2),
 ]
 
 render(world)
