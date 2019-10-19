9b71f6508723a1c26f900e0c45310cfbe3a847cd
Amy <amy@fluff.tech>
2019-10-18 22:28:46 -0400

    replace sphere object with arrays

--- tracer.rb
+++ tracer.rb
@@ -20,39 +20,27 @@ end
 def norm(a)
 	div_vec(a,Math.sqrt(abs(a)))
 end
-
-class Sphere
-	attr_reader :center, :radius, :color1_vec, :color2_vec, :reflect
-	def initialize(center, radius, color1_vec, color2_vec, reflect)
-		@center = center
-		@radius = radius
-		@color1_vec = color1_vec
-		@color2_vec = color2_vec
-		@reflect = reflect
-	end
-	
-	def intersect(ray_orig, ray_dir)
-		l = sub_vec(center, ray_orig)
-		tca = dot_vec(l, ray_dir)
-		return 1e8 if tca < 0
-		d2 = abs(l) - tca ** 2
-		return 1e8 if d2 > radius ** 2
-		thc = Math.sqrt(radius ** 2 - d2)
-		return [tca - thc, tca + thc].min
-	end
-	def color(intersect)
-		(intersect[0].floor % 2) == (intersect[2].floor % 2) ? color1_vec : color2_vec
-	end
+def intersect(sphere, ray_orig, ray_dir)
+	l = sub_vec(sphere[0], ray_orig)
+	tca = dot_vec(l, ray_dir)
+	return 1e8 if tca < 0
+	d2 = abs(l) - tca ** 2
+	return 1e8 if d2 > sphere[1] ** 2
+	thc = Math.sqrt(sphere[1] ** 2 - d2)
+	[tca - thc, tca + thc].min
+end
+def color(sphere, intersect)
+	(intersect[0].floor % 2) == (intersect[2].floor % 2) ? sphere[2] : sphere[3]
 end
 
 def raytrace(ray_orig, ray_dir, world, depth = 0)
 	
-	nearest_obj, min_dist = world.to_h{|s| [s, s.intersect(ray_orig, ray_dir)]}.min_by{|k,v| v}
+	sphere, min_dist = world.to_h{|s| [s, intersect(s, ray_orig, ray_dir)]}.min_by{|k,v| v}
 	
 	return [0.5]*3 if min_dist >= 1e8
 	
 	intersect = add_vec(ray_orig, mul_vec(ray_dir, min_dist))
-	normal = norm(div_vec(sub_vec(intersect, nearest_obj.center), nearest_obj.radius))
+	normal = norm(div_vec(sub_vec(intersect, sphere[0]), sphere[1]))
 	
 	color = [0.05]*3
 	
@@ -63,27 +51,27 @@ def raytrace(ray_orig, ray_dir, world, depth = 0)
 	
 	offset = add_vec(intersect, mul_vec(normal, 1e-4))
 	
-	light_distances = world.map { |obj| obj.intersect(offset, light_dir) }
-	light_visible = light_distances[world.index(nearest_obj)] == light_distances.min
+	light_distances = world.map {|s| intersect(s, offset, light_dir)}
+	light_visible = light_distances[world.index(sphere)] == light_distances.min
 	
 	lv = [0, dot_vec(normal, light_dir)].max
-	color = add_vec(color, mul_vec(nearest_obj.color(intersect), lv)) if light_visible
+	color = add_vec(color, mul_vec(color(sphere, intersect), lv)) if light_visible
 	
-	if nearest_obj.reflect > 0 && depth < 5
+	if sphere[4] > 0 && depth < 5
 		reflect_ray_dir = norm(sub_vec(ray_dir, mul_vec(normal, 2 * dot_vec(ray_dir, normal))))
-		color = add_vec(color, mul_vec(raytrace(offset, reflect_ray_dir, world, depth + 1), nearest_obj.reflect))
+		color = add_vec(color, mul_vec(raytrace(offset, reflect_ray_dir, world, depth + 1), sphere[4]))
 	end
 	
 	phong = dot_vec(normal, norm(add_vec(light_dir, origin_dir)))
 	color = add_vec(color, mul_vec([1]*3, phong.clamp(0, 1) ** 50)) if light_visible
 	
-	return color
+	color
 end
 
 world = [
-	Sphere.new([0, -10004, 20], 10000, [0.25]*3, [0]*3, 0.2),
-	Sphere.new([0, 0, 20], 4, [1, 0, 0], [1, 0, 0], 0.2),
-	Sphere.new([6, -1, 20], 2, [0, 0, 1], [0, 0, 1], 0.2),
+	[[0, -10004, 20], 10000, [0.25]*3, [0]*3, 0.2],
+	[[0, 0, 20], 4, [1, 0, 0], [1, 0, 0], 0.2],
+	[[6, -1, 20], 2, [0, 0, 1], [0, 0, 1], 0.2],
 ]
 
 aspect_ratio = 4/3.0
