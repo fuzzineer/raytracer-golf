688a6e37fe999d63dcd1c242a1dffaf56abca46a
Amy <amy@fluff.tech>
2019-10-25 21:15:43 -0400

    annihilate variable names

--- tracer.rb
+++ tracer.rb
@@ -17,58 +17,58 @@ class Array
 	end
 end
 
-V=->n{
-	[n,n,n]
+V=->f{
+	[f,f,f]
 }
 N=->v{
 	v/sqrt(v%v)
 }
-I=->sphere,ray_orig,ray_dir{
-	l=sphere[0]-ray_orig
-	tca=l%ray_dir
-	d2=l%l-tca**2
-	return 1e9 if tca<0||d2>sphere[1]**2
-	thc=sqrt sphere[1]**2-d2
-	[tca-thc,tca+thc].min
+I=->s,o,d{
+	l=s[0]-o
+	t=l%d
+	u=l%l-t**2
+	return 1e9 if t<0||u>s[1]**2
+	h=sqrt s[1]**2-u
+	[t-h,t+h].min
 }
-C=->x{
-	x.clamp 0,1
-}
-
-R=->ray_orig,ray_dir,depth{
-	
-	sphere,min_dist=WORLD.map{|s|[s,I[s,ray_orig,ray_dir]]}.min_by{|k,v|v}
-	
-	return[0.4,0.8,1]*(1-ray_dir[1])**3 if min_dist>1e8
-	
-	intersect=ray_orig+ray_dir*min_dist
-	normal=N[(intersect-sphere[0])/sphere[1]]
-	
-	color=V[0.05]
-	
-	light_dir=N[[-20,40,10]-intersect]
-	
-	offset=intersect+normal*1e-4
-	
-	light_distances=WORLD.map{|s|I[s,offset,light_dir]}
-	
-	light_distances[WORLD.index(sphere)]==light_distances.min&&color+=sphere[!sphere[3]||intersect[0].ceil%2==intersect[2].ceil%2?2:3]*[0,normal%light_dir].max+V[1]*C[normal%N[light_dir+N[V[0]-intersect]]]**50
-	
-	depth<3&&color+=R[offset,N[ray_dir-normal*2*(ray_dir%normal)],depth+1]*0.3
-	
-	color
+C=->f{
+	f.clamp 0,1
 }
 
-WORLD=[[[0,-10002,20],1e4,V[0.25],V[0]]]
+R=->o,d,b{
+	
+	s,t=W.map{|s|[s,I[s,o,d]]}.min_by{|s,t|t}
+	
+	return[0.4,0.8,1]*(1-d[1])**3 if t>1e8
+	
+	i=o+d*t
+	n=N[(i-s[0])/s[1]]
+	
+	c=V[0.05]
+	
+	l=N[[-20,40,10]-i]
+	
+	k=i+n*1e-4
+	
+	m=W.map{|s|I[s,k,l]}
+	
+	m[W.index(s)]==m.min&&c+=s[!s[3]||i[0].ceil%2==i[2].ceil%2?2:3]*[0,n%l].max+V[1]*C[n%N[l+N[V[0]-i]]]**50
+	
+	b<3&&c+=R[k,N[d-n*2*(d%n)],b+1]*0.3
+	
+	c
+}
+
+W=[[[0,-10002,20],1e4,V[0.25],V[0]]]
 
 0.step(25,PI/4){|t|
-	WORLD<<[[t-14.5,2**(-0.1*t)*sin(t/2).abs*10-1.35,37-t],0.7,[0,2,4].map{|i|C[cos(t/PI+i)]}]
+	W<<[[t-14.5,2**(-0.1*t)*sin(t/2).abs*10-1.35,37-t],0.7,[0,2,4].map{|i|C[cos(t/PI+i)]}]
 }
 
-angle=15/56r
+A=15/56r
 
 $><<"P3 640 480 255"
 
-[*0..479].product([*0..639]){|row,col|
-	R[V[0],N[[(2.*(col+0.5)/640-1)*angle*4/3,(1-2.*(row+0.5)/480)*angle,1]],0].map{|c|$><<" "<<(C[c]*255).to_i}
+[*0..479].product([*0..639]){|r,c|
+	R[V[0],N[[(2.*(c+0.5)/640-1)*A*4/3,(1-2.*(r+0.5)/480)*A,1]],0].map{|c|$><<" "<<(C[c]*255).to_i}
 }
